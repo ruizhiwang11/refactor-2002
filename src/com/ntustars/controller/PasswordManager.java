ca87b7debf3b067cb416d5fb2fb83105f6eb9241
ruizhi <ruizhiwang11@gmail.com>
2020-11-22 12:49:43 +0800

    rework Password Manager

--- src/com/ntustars/controller/PasswordManager.java
+++ src/com/ntustars/controller/PasswordManager.java
@@ -1,57 +1,71 @@
 package com.ntustars.controller;
 
+import java.io.UnsupportedEncodingException;
 import java.security.GeneralSecurityException;
 import java.security.Key;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
 import java.util.Base64;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
+import javax.crypto.spec.SecretKeySpec;
+
 public class PasswordManager
 {
-    private static Cipher cipher ;
-
-    private static Key key = Generator();
-    private  static KeyGenerator keyGenerator;
-    public static String encrypt(String text){
-        byte[] encryptedByte;
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
+    private static final String secret = "ssshhhhhhhhhhh!!!!";
+    public static void setKey(String myKey)
+    {
+        MessageDigest sha = null;
         try {
-            cipher = Cipher.getInstance("DES");
-            byte[] plainTextByte = text.getBytes();
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            encryptedByte = cipher.doFinal(plainTextByte);
-            Encoder encoder = Base64.getEncoder(); //ERROR "cannot resolve method"
-            String encryptedText = encoder.encodeToString(encryptedByte);
-            return encryptedText;
-        }catch (GeneralSecurityException e){
+            key = myKey.getBytes("UTF-8");
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, "AES");
+        }
+        catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
+        }
+        catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String encrypt(String strToEncrypt)
+    {
+        try
+        {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error while encrypting: " + e.toString());
         }
         return null;
     }
-    public static String decrypt(String text){
-        try{
-            cipher = Cipher.getInstance("DES");
-            Decoder decoder = Base64.getDecoder(); //ERROR "cannot resolve method"
-            byte[] encryptedTextByte = (byte[]) decoder.decode(text);
-            cipher.init(Cipher.DECRYPT_MODE, key);
-            byte[] decryptedByte = cipher.doFinal(encryptedTextByte);
-            String decryptedText = new String(decryptedByte);
-            return decryptedText;
-        }catch (GeneralSecurityException e){
-            e.printStackTrace();
-        }
-        return  null;
 
-    }
-    private static Key Generator() {
-        try {
-            keyGenerator = KeyGenerator.getInstance("DES");
-            keyGenerator.init(56);
-            key = keyGenerator.generateKey();
-        } catch (Exception ex) {
-            ex.printStackTrace();
+    public static String decrypt(String strToDecrypt)
+    {
+        try
+        {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
         }
-        return key;
+        catch (Exception e)
+        {
+            System.out.println("Error while decrypting: " + e.toString());
+        }
+        return null;
     }
     public static void main(String[] args) throws Exception
     {
