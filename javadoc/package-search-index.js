ec4df28b1fffd3e3d85b67515e1a944342101958
Your Name <you@example.com>
2020-11-24 23:50:34 +0700

    add javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.ntustars"},{"l":"com.ntustars.Boundary"},{"l":"com.ntustars.controller"},{"l":"com.ntustars.entity"}];updateSearchResults();
